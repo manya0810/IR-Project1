3cb1cec37bec127174292a950994e46318d089bc
manya0810 <66786993+manya0810@users.noreply.github.com>
2021-09-20 00:12:10 -0400

    Update indexer.py

--- project1/indexer.py
+++ project1/indexer.py
@@ -146,7 +146,7 @@ class Indexer:
                 }
             ]
         }
-        requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json()
+         print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
 
 
 if __name__ == "__main__":
